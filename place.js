a8f3e7419f2695f2e212376b7acbc3b0480308bd
Kizer27 <kizersam000@hsestudents.org>
2025-11-25 13:54:08 -0500

    Add files via upload

--- place.js
+++ place.js
@@ -1,12 +1,16 @@
-function dcList(){
-        let newDC = document.createElement("li");
-        newDC.innerHTML = prompt("What's your favorite attraction in DC?");
-        document.getElementById("attractions").appendChild(newDC);
-}
-
 document.addEventListener('DOMContentLoaded', start) 
 function start(){
     let capitol = document.getElementById("opener");
     let map = document.createElement("img");
-    map.style.src = "https://upload.wikimedia.org/wikipedia/commons/f/f5/Map_of_usa_highlighting_dc.png"
+    map.src = "https://upload.wikimedia.org/wikipedia/commons/f/f5/Map_of_usa_highlighting_dc.png";
+    capitol.after(map);
+
+    document.body.style.color = "darkgreen";
+    document.body.style.backgroundColor = "lightgrey";
+}
+
+function dcList(){
+        let newDC = document.createElement("li");
+        newDC.innerHTML = prompt("What's your favorite attraction in DC?");
+        document.getElementById("attractions").appendChild(newDC);
 }
